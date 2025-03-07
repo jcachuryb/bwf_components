41f46eb7386d4cfabfcffb47bdb324f031404ebd
Juan Achury <jcachuryb@outlook.com>
2025-03-07 09:09:27 +0000

    Context Menu element for easy assingment

--- bwf_components/static/common/js/value-selector-plugin/value_selector.js
+++ bwf_components/static/common/js/value-selector-plugin/value_selector.js
@@ -107,9 +107,9 @@ class ValueSelector {
       const popoverContent = $('[data-name="popover-content"]').clone();
       popoverContent.attr("data-name", null);
       popoverContent.attr("id", `popover-content-${input.key}`);
-      // popoverContent
-      //   .find(".context-menu")
-      //   .attr("id", `context-menu-${input.key}`);
+      popoverContent
+        .find(".context-menu")
+        .attr("id", `context-menu-${input.key}`);
       popoverContent.find(".btn-save").on("click", _, function (event) {
         const selector = event.data;
         const selectedValue = selector.editor.getValue();
@@ -124,7 +124,6 @@ class ValueSelector {
       const popoverOptions = {
         html: true,
         title: "",
-        lineNumbers: true,
         content: popoverContent,
         placement: "top",
       };
@@ -136,21 +135,8 @@ class ValueSelector {
       _.$element.on("shown.bs.popover", _, function (event) {
         const _ = event.data;
         const { input, component } = _;
-        // $(`#context-menu-${input.key}`).menu();
-        // $(`#context-menu-${input.key}`).show();
-
-        if (!_.editor) {
-          _.editor = CodeMirror($(`#editor-${input.key}`)[0], {
-            doc: "Start document",
-            value: value,
-            mode: "python",
-            placeholder: "Enter a value",
-            theme: "default",
-            lineNumbers: true,
-            styleActiveLine: true,
-            matchBrackets: true,
-          });
-        }
+        _.setupMenu();
+        // _.setUpEditor();
       });
       _.$element.on("show.bs.popover", _, function (event) {
         $(".popover.show").each(function () {
@@ -172,15 +158,126 @@ class ValueSelector {
   onPopoverOpen() {
     const _ = this;
   }
+  setupMenu() {
+    const _ = this;
+    const { markup } = utils;
+    const { input, component } = _;
+    const value = input.value;
+    const { type, options, value_rules } = _.input?.json_value ?? {};
 
-  saveValue(selectedValue) {
+    const menuElements = markup("ul", [
+      // inputs,
+      {
+        tag: "li",
+        content: [
+          markup("div", "Inputs"),
+          markup(
+            "ul",
+            workflow_inputs.var.inputs.map((el) => ({
+              tag: "li",
+              content: el.label,
+              class: "",
+              "data-value": el.id,
+              "data-key": el.key,
+            }))
+          ),
+        ],
+      },
+      // variables
+      {
+        tag: "li",
+        content: [
+          markup("div", "Variables"),
+          markup(
+            "ul",
+            workflow_variables.var.variables.map((el) => ({
+              tag: "li",
+              content: el.name,
+              class: "",
+              "data-value": el.id,
+              "data-key": el.key,
+            }))
+          ),
+        ],
+      },
+    ]);
+
+    $(`#context-menu-${input.key}`).empty();
+    $(`#context-menu-${input.key}`).append(menuElements.children);
+    $(`#context-menu-${input.key}`).menu();
+    $(`#context-menu-${input.key}`).show();
+  }
+  setUpEditor() {
+    const _ = this;
+    const { input, component } = _;
+    const value = input.value;
+    const { type, options, value_rules } = _.input?.json_value ?? {};
+    if (!_.editor) {
+      _.editor = CodeMirror.fromTextArea($(`#editor-${input.key}`)[0], {
+        doc: "Start document",
+        value: value,
+        mode: "python",
+        placeholder: "Enter a value",
+        theme: "default",
+        lineNumbers: true,
+        styleActiveLine: true,
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        lineWrapping: true,
+      });
+
+      _.editor.setOption("extraKeys", {
+        "Ctrl-Space": "autocomplete",
+        "Ctrl-Enter": function (cm) {
+          _.saveValue(cm.getValue());
+        },
+      });
+    }
+
+    function getHints(cm, option) {
+      return new Promise(function (accept) {
+        setTimeout(function () {
+          var cursor = cm.getCursor(),
+            line = cm.getLine(cursor.line);
+          var start = cursor.ch,
+            end = cursor.ch;
+          while (start && /(\$|\w)/.test(line.charAt(start - 1))) --start;
+          while (end < line.length && /(\$)\w/.test(line.charAt(end))) ++end;
+          var word = line.slice(start, end).toLowerCase();
+          if (word === "" || word.startsWith("$")) return accept(null);
+
+          const vars = workflow_variables.var.variables
+            .filter((v) => v.key.startsWith(word))
+            .map((v) => `${v.context}['${v.key}']`);
+
+          const inputs = workflow_inputs.var.inputs
+            .filter((v) => v.key.startsWith(word))
+            .map((v) => `$inputs['${v.key}']`);
+          const local = [];
+
+          // TODO: Get values from output
+          return accept({
+            list: vars.concat(inputs),
+            from: CodeMirror.Pos(cursor.line, start),
+            to: CodeMirror.Pos(cursor.line, end),
+          });
+        }, 100);
+      });
+    }
+
+    _.editor.setOption("hintOptions", {
+      hint: getHints,
+    });
+  }
+
+  saveValue(value) {
     const { input, component, $element, popover } = this;
     const data = {
       id: component.id,
       plugin_id: component.plugin_id,
       plugin_version: component.plugin_version,
       key: input.key,
-      value: selectedValue,
+      value: value,
     };
     workflow_components.api.updateComponentInputValue(
       data,
